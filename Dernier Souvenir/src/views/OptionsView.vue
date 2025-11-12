0ce8ab9404859489942498eaf37de2956e93e416
Émeryk Bélisle <emeryk.belisle@hotmail.com>
2025-11-12 10:34:13 -0500

    Css fait pour la page option

--- Dernier Souvenir/src/views/OptionsView.vue
+++ Dernier Souvenir/src/views/OptionsView.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="options-view-wrapper">
     <h1>Options</h1>
-    <h3>Volume</h3>
+    <h3>Volume - {{ volume }}</h3>
     <input type="range" min="0" max="100" v-model="volume" />
-    <h3>Vitesse des textes</h3>
-    <input type="range" min="0" max="100" v-model="textSpeed" /> <br />
+    <h3>Vitesse des textes - {{ textSpeed }}</h3>
+    <input type="range" min="0" max="10" v-model="textSpeed" /> <br />
     <move-view-button label="Retour" page="" />
   </div>
 </template>
 
 <script setup>
 import MoveViewButton from "@/components/common/MoveViewButton.vue";
+import { ref } from "vue";
+
+const volume = ref(50);
+const textSpeed = ref(50);
 </script>
 
 <style scoped>
@@ -23,6 +27,7 @@ import MoveViewButton from "@/components/common/MoveViewButton.vue";
   align-items: center;
   height: 100vh;
   gap: 20px;
+  animation: fadeIn 1.5s ease-in-out forwards;
 }
 
 h1 {
@@ -33,7 +38,24 @@ h1 {
   margin: 0;
   color: #ffc973;
   text-shadow: 5px 5px 4px #000000, 0 0 10px rgba(255, 201, 115, 0.3);
-  animation: fadeIn 1.5s ease-in-out forwards,
-    pulse 8s ease-in-out 1.5s infinite;
+  animation: pulse 8s ease-in-out 1.5s infinite;
+}
+
+h3 {
+  font-size: 2vw;
+  color: #ffc973;
+  text-shadow: 2px 2px 4px #000000, 0 0 10px rgba(255, 201, 115, 0.3);
+  margin-bottom: 0;
+}
+
+input[type="range"] {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 20vw;
+  border-radius: 50px;
+  background: #503c20;
+  accent-color: #ffc973;
+  cursor: pointer;
+  box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2);
 }
 </style>
